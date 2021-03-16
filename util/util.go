90a0d681b550e18f4ea867fa855de5e7b04ffcc6
Alice Pote <alice.writes.wrongs@gmail.com>
2021-03-16 15:32:03 -0400

    remove backet hack

--- util/util.go
+++ util/util.go
@@ -5,7 +5,6 @@ import (
 	"io"
 	"log"
 	"os"
-	"regexp"
 )
 
 func CheckErr(err error) {
@@ -28,14 +27,7 @@ func Copy(src, dst string) error {
 	return out.Close()
 }
 
-var leftBracketRegex = regexp.MustCompile(`\[`)
-var rightBracketRegex = regexp.MustCompile(`\]`)
-
 func DecodeWindows1250(input []byte) ([]byte, error) {
 	utf8, err := charmap.Windows1250.NewDecoder().Bytes(input)
-
-	leftReplaced := leftBracketRegex.ReplaceAllString(string(utf8), "(")
-	rightReplaced := rightBracketRegex.ReplaceAllString(leftReplaced, ")")
-
-	return []byte(rightReplaced), err
+	return utf8, err
 }
