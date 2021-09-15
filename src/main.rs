54baa89939d1048a9bafa497240277af75488886
Alice Pote <alice.writes.wrongs@gmail.com>
2021-09-15 09:36:23 -0400

    trying out an argument parsing library - it's nice!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use clap::{AppSettings, Clap};
+
+#[derive(Clap)]
+#[clap(name = "wget2hugo", version = "0.0.1")]
+#[clap(setting = AppSettings::ColoredHelp)]
+struct Options {
+    #[clap(short, long)]
+    source: String,
+
+    #[clap(short, long)]
+    destination: String,
+
+    #[clap(short, long)]
+    convert_from_1252: Option<bool>,
+}
+
 fn main() {
+    let opts: Options = Options::parse();
+
     println!("Hello, world!");
+    println!("source: {}", opts.source);
+    println!("dest: {}", opts.destination);
 }
