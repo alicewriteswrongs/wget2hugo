78e63fc4470429e5d26ca31ef477485d007c34fc
Alice Pote <alice.writes.wrongs@gmail.com>
2021-09-16 10:10:43 -0400

    got some markdown conversion working, automatic encoding detection

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
+use chardetng::EncodingDetector;
 use clap::{AppSettings, Clap};
 use html2md::parse_html;
-use std::ffi::OsStr;
+use std::fs;
 use walkdir::WalkDir;
 
 #[derive(Clap)]
@@ -12,45 +13,51 @@ struct Options {
 
     #[clap(short, long)]
     destination: String,
+}
 
-    #[clap(short, long)]
-    convert_from_1252: Option<bool>,
+fn convert_to_utf8(bytes: Vec<u8>) -> String {
+    let mut detector = EncodingDetector::new();
+    let slice = bytes.as_slice();
+    detector.feed(slice, true);
+    let encoding = detector.guess(None, true);
+    let (converted, _used, _malformed) = encoding.decode(slice);
+    String::from(converted)
+}
+
+// Take an HTML string and convert it to Markdown using
+// html2md::parse_html
+fn convert_to_markdown(html: String) -> String {
+    parse_html(&html)
 }
 
 fn main() {
     let opts: Options = Options::parse();
 
-    println!("Hello, world!");
-    println!("source: {}", opts.source);
-    println!("dest: {}", opts.destination);
-
-    // let foo = WalkDir::new(opts.source);
-
     for entry in WalkDir::new(opts.source) {
         let direntry = entry.unwrap();
         let path = direntry.path();
 
-        let extension = path.extension().map(|os| os.to_str());
+        let extension = path.extension().map(|os| os.to_str().unwrap());
 
         match extension {
             // we've got a directory
             None => {
                 println!("found directory: {}", path.display());
-            },
+            }
             // we've got a file (or something with an extension!)
-            Some(string_ext) => {
-                match string_ext {
-                    Some("htm") | Some("html") => {
-                        println!("found html {}", path.display());
-                    }
-                    Some(ext) => {
-                        println!("another ext:  {}", ext);
-                        println!("at path: {}", path.display());
-                    },
-                    None => {
-                        println!("something went wrong");
-                    }
+            Some("htm") | Some("html") => {
+                println!("found html {}", path.display());
+
+                let markdown = fs::read(path).map(convert_to_utf8).map(convert_to_markdown);
+
+                match markdown {
+                    Ok(md) => println!("converted! {}", md),
+                    Err(err) => println!("some error {}", err),
                 }
+            }
+            Some(ext) => {
+                println!("another ext:  {}", ext);
+                println!("at path: {}", path.display());
             }
         }
     }
