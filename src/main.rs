c1857700212fd658aca7e320aca6dc5d13e80c0e
Alice Pote <alice.writes.wrongs@gmail.com>
2021-09-17 09:01:50 -0400

    get the basic file copying working

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use clap::{AppSettings, Clap};
 use std::fs;
+use std::path::Path;
 use walkdir::WalkDir;
 
 mod conversion;
@@ -18,34 +19,42 @@ struct Options {
 fn main() {
     let opts: Options = Options::parse();
 
-    for entry in WalkDir::new(opts.source) {
+    let source_dir_path = Path::new(&opts.source);
+    let destination_dir_path = Path::new(&opts.destination);
+
+    for entry in WalkDir::new(&opts.source) {
+        // I think it's ok to unwrap this here since we
+        // don't really mind if this panics (if it does, we want
+        // to not do anything anyhow)
         let direntry = entry.unwrap();
-        let path = direntry.path();
+        let source_path = direntry.path();
 
-        let extension = path.extension().map(|os| os.to_str().unwrap());
+        let destination_path = source_path
+            .strip_prefix(source_dir_path)
+            .map(|relative_path| destination_dir_path.join(relative_path));
 
-        match extension {
-            // we've got a directory
-            None => {
-                println!("found directory: {}", path.display());
+        match destination_path {
+            Ok(mut destination_path) => {
+                println!("source path:      {}", source_path.display());
+                println!("destination path: {}", destination_path.display());
+
+                let extension = destination_path.extension().map(|os| os.to_str().unwrap());
+
+                let operation = match extension {
+                    // we've got a directory
+                    None => fs::create_dir(destination_path),
+                    // we've got a file (or something with an extension!)
+                    Some("htm") | Some("html") => fs::read(source_path)
+                        .map(conversion::bytes_to_utf8)
+                        .map(conversion::html_to_markdown)
+                        .map(|markdown| {
+                            destination_path.set_extension("md");
+                            fs::write(destination_path, markdown).unwrap()
+                        }),
+                    Some(_ext) => fs::copy(source_path, destination_path).map(|_number| ()),
+                };
             }
-            // we've got a file (or something with an extension!)
-            Some("htm") | Some("html") => {
-                println!("found html {}", path.display());
-
-                let markdown = fs::read(path)
-                    .map(conversion::bytes_to_utf8)
-                    .map(conversion::html_to_markdown);
-
-                match markdown {
-                    Ok(md) => println!("converted! {}", md),
-                    Err(err) => println!("some error {}", err),
-                }
-            }
-            Some(ext) => {
-                println!("another ext:  {}", ext);
-                println!("at path: {}", path.display());
-            }
+            Err(error) => println!("some error: {}", error),
         }
     }
 }
